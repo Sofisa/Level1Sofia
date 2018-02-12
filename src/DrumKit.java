ad2685d595e63d786446c0169d5470ad8011e4e6
Sofisa <league@WorthyMini-6.attlocal.net>
2018-02-11 16:32:34 -0800

    DrumKit

--- src/DrumKit.java
+++ src/DrumKit.java
@@ -16,6 +16,7 @@ public class DrumKit extends MouseAdapter {
 
     JLabel drumLabelWithImage;
     JLabel drumTwoWithImage;
+    JLabel drumThreeWithImage;
 
     public static void main(String[] args) throws Exception {
    	 new DrumKit().getGoing();
@@ -27,7 +28,7 @@ public class DrumKit extends MouseAdapter {
    	 // 2. Make the frame visible
   window.setVisible(true);
    	 // 3. Set the size of the frame
- 	 window.setSize(700,700);
+ 	 window.setSize(200,200);
    	 // 4. Set the title of the frame
    	 window.setTitle("Drum Kit");
    	 // 5. Make a JPanel and initialize it.
@@ -38,18 +39,23 @@ public class DrumKit extends MouseAdapter {
    	 // 8. Put the name of your image in a String variable.
     String drumpic = "drum1.jpg";
     String drumpictwo = "drum2.jpg";
+    String drumpicthre = "drumpicthre.jpg";
    	 // 9. Edit the next line to use your String variable
  drumLabelWithImage = createLabelImage(drumpic);
    	 drumTwoWithImage = createLabelImage(drumpictwo);
+   	 drumThreeWithImage = createLabelImage(drumpicthre);
    	 // 10. Add the image to the panel
     panel.add(drumLabelWithImage);
     panel.add(drumTwoWithImage);
+    panel.add(drumThreeWithImage);
    	 // 11. Set the layout of the panel to "new GridLayout()"
     panel.setLayout(new GridLayout());
    	 // 12. call the pack() method on the frame
    	 window.pack();
    	 // 13. add a mouse listener to drumLabelWithImage.
 drumLabelWithImage.addMouseListener(this);
+drumTwoWithImage.addMouseListener(this);
+drumThreeWithImage.addMouseListener(this);
     }
 
     public void mouseClicked(MouseEvent e) {
@@ -58,8 +64,14 @@ System.out.println("hi");
    	 JLabel drumClicked = (JLabel) e.getSource();
    	 // 15. Download a drum sound and drop it into your "default package". You can find it on freesound.org. To download it, log in as leagueofamazing/code4life.
    	 // 16. If they clicked on the drumImage...
-if(drumClicked == drumLabelWithImage); {
+if(drumClicked == drumLabelWithImage) {
 	playSound("snaredrum.wav");
+}
+if(drumClicked == drumTwoWithImage) {
+	playSound("bump.aiff");
+}
+if(drumClicked == drumThreeWithImage) {
+	playSound("drum.aiff");
 }
    		 // 17. ...use the playSound method to play a drum sound.
 
